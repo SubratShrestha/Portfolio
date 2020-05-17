5def1398c5e829cfdabbe8ee5136d10f2a7799a6
subrat shrestha <z5266273@ad.unsw.edu.au>
2020-05-17 17:45:19 +1000

    added section for allowance

--- resources/JS/script.js
+++ resources/JS/script.js
@@ -2,10 +2,10 @@
 /*_______________________________NAVIGATION.__________________________________*/
 /*____________________________________________________________________________*/
 
-$(document).ready(function() {
+$(document).ready(function () {
     const nav = document.querySelector('.navi');
     const topOfNav = nav.offsetTop;
-    $('.navi').waypoint(function(direction) {
+    $('.navi').waypoint(function (direction) {
         if (direction == "down") {
             document.body.style.paddingTop = nav.offsetHeight + 'px';
             $('.navi').addClass('sticky');
@@ -16,21 +16,37 @@ $(document).ready(function() {
         }
     })
 
-    $('.flavoury').waypoint(function(direction) {
+    $('.budgetor').waypoint(function (direction) {
         if (direction == 'down') {
-            $('.flavoury__indicator').addClass('active');
+            $('.budgetor__indicator').addClass('active');
+            $('.flavoury__indicator').removeClass('active');
             $('.riskDice__indicator').removeClass('active');
         } else {
+            $('.budgetor__indicator').removeClass('active');
             $('.flavoury__indicator').removeClass('active');
             $('.riskDice__indicator').removeClass('active');
         }
-    },{ offset: '320px;' })
+    }, { offset: '320px;' })
 
-    $('.riskDice').waypoint(function(direction) {
+    $('.flavoury').waypoint(function (direction) {
+        if (direction == 'down') {
+            $('.flavoury__indicator').addClass('active');
+            $('.budgetor__indicator').removeClass('active');
+            $('.riskDice__indicator').removeClass('active');
+        } else {
+            $('.budgetor__indicator').addClass('active');
+            $('.flavoury__indicator').removeClass('active');
+            $('.riskDice__indicator').removeClass('active');
+        }
+    }, { offset: '100px;' })
+
+    $('.riskDice').waypoint(function (direction) {
         if (direction == 'down') {
             $('.riskDice__indicator').addClass('active');
             $('.flavoury__indicator').removeClass('active');
+            $('.budgetor__indicator').removeClass('active');
         } else {
+            $('.budgetor__indicator').removeClass('active');
             $('.riskDice__indicator').removeClass('active');
             $('.flavoury__indicator').addClass('active');
         }
